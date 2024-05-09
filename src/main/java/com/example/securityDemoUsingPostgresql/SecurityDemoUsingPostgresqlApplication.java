2b4bcabd9400b41e7097fafcca6d043bbc0454e0
Suhaib Khan <suhaibkhan@Suhaibs-MacBook-Pro.local>
2024-05-09 13:15:29 +0530

    Initial commit

---
+++ src/main/java/com/example/securityDemoUsingPostgresql/SecurityDemoUsingPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityDemoUsingPostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoUsingPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemoUsingPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/securityDemoUsingPostgresql/models/ERole.java
@@ -0,0 +1,7 @@
+package com.example.securityDemoUsingPostgresql.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
